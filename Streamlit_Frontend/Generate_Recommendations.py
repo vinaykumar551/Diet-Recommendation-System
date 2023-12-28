1a0132ef0a749406b1df7464362796d668f792fd
vinaykumar551 <112416282+vinaykumar551@users.noreply.github.com>
2023-12-28 11:16:15 +0530

    Update Generate_Recommendations.py

--- Streamlit_Frontend/Generate_Recommendations.py
+++ Streamlit_Frontend/Generate_Recommendations.py
@@ -18,5 +18,5 @@ class Generator:
             'ingredients':self.ingredients,
             'params':self.params
         }
-        response=requests.post(url='FastAPI_Backend/main.py',data=json.dumps(request))
+        response=requests.post(url='http://localhost:8000/predict/'',data=json.dumps(request))
         return response
